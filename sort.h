9e18c9d3e32c681fd77cbea18e359872bbb9dc24
Brehyma Traore <brehymatraore50@gmail.com>
2024-04-30 15:07:48 -0700

    Same file removed

--- sort.h
+++ sort.h
@@ -23,16 +23,10 @@ typedef struct listint_s
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-void print_bubble(const int *array, size_t size);
 
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
-/* From printf */
-int _printf(const char *format, ...);
-ssize_t print_int(int n);
-void print_list(const listint_t *list);
 
 #endif /*SORT_H*/
